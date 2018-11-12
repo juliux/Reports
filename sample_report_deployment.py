f185ee73e170eef18534c43ea55ec7e21f883602
Juliux Rivera <julio.adalberto.rivera@ericsson.com>
2018-11-12 23:46:06 +0100

    Separate files for Postgres & RDS, added ALTER for functions + GRANTS
    Signed-off-by: Juliux Rivera <julio.adalberto.rivera@ericsson.com>

--- sample_report_deployment.py
+++ sample_report_deployment.py
@@ -27,6 +27,7 @@ EMPTY_SPACE = ""
 # - Partition stacking
 
 CURRENT_PARTITION_QUERY = "SELECT a.currentpart FROM rds$datepart_data a INNER JOIN rds$datepartitioning b on b.id = a.id WHERE b.tablename = 'report$reservation';"
+CURRENT_SCHEMAS_QUERY = "SELECT DISTINCT schemaname FROM pg_catalog.pg_views WHERE schemaname NOT IN ('pg_catalog', 'information_schema') ORDER BY schemaname;"
 TABLE_NAME_EOD_SUM_REPORT = "reporting$eod_trans_sum_table_p"
 
 # - EOD Table stacking
@@ -62,7 +63,108 @@ STATIC_TABLE_PART_EOD_SUMM_3 = """  ELSE
 END;
 $_$;
 
-CREATE TRIGGER "reporting$eod_trans_sum_table_trigger" BEFORE INSERT ON "reporting$eod_trans_sum_table" FOR EACH ROW EXECUTE PROCEDURE "reporting$eod_trans_sum_insert_function"();"""
+CREATE TRIGGER "reporting$eod_trans_sum_table_trigger" BEFORE INSERT ON "reporting$eod_trans_sum_table" FOR EACH ROW EXECUTE PROCEDURE "reporting$eod_trans_sum_insert_function"();
+
+"""
+
+STATIC_FUNCTION_INSERT = """CREATE FUNCTION "end_of_day_transaction_summary_insert"(starttime date, endtime date) RETURNS VOID
+  LANGUAGE SQL
+  AS $_$
+    INSERT INTO reporting$eod_trans_sum_table
+    ( insert_date,start_date,end_date,operation,total_count,currency,total_amount,total_fees,total_discounts,total_promotions,total_coupons,additional_info )
+    (
+      select
+        now()::date                                                     as insert_date,
+        starttime                                                       as start_date,
+        endtime                                                         as end_date,
+        operationtype                                                   as operation,
+        count(*)                                                        as total_count,
+        currencycode                                                    as currency,
+        sum(coalesce(amount, 0.0))                                      as total_amount,
+        sum(coalesce(fromexternalfee, 0.0))
+                    + sum(coalesce(frominternalfee, 0.0))
+                    + sum(coalesce(toexternalfee, 0.0))
+                    + sum(coalesce(tointernalfee, 0.0))                 as total_fees,
+        sum(coalesce(fromdiscount, 0.0))
+                    + sum(coalesce(todiscount, 0.0))                    as total_discounts,
+        sum(coalesce(frompromotion, 0.0))
+                    + sum(coalesce(topromotion, 0.0))                   as total_promotions,
+        null::numeric                                                   as total_coupons,
+        toserviceprovider                                               as additional_info
+      from "global".committed_financialevent(starttime, endtime)
+      group by operationtype, currencycode, toserviceprovider
+      order by operationtype, currencycode, toserviceprovider
+    );
+  $_$;
+"""
+
+STATIC_NEW_EOD_FUNCTION_GENERAL = """ RETURNS TABLE(start_date date, end_date date, operation text, total_count numeric, currency text, total_amount numeric, total_fees numeric, total_discounts numeric, total_promotions numeric, total_coupons numeric, additional_info text)
+ LANGUAGE sql
+ STABLE SECURITY DEFINER
+AS $function$
+  select
+    starttime                                                       as start_date,
+    endtime                                                         as end_date,
+    operation                                                       as operation,
+    sum(total_count)                                                as total_count,
+    currency                                                        as currency,
+    sum(coalesce(total_amount, 0.0))                                as total_amount,
+    sum(coalesce(total_fees, 0.0))                                  as total_fees,
+    sum(coalesce(total_discounts, 0.0))                             as total_discounts,
+    sum(coalesce(total_promotions, 0.0))                            as total_promotions,
+    null::numeric                                                   as total_coupons,
+    additional_info                                                 as additional_info
+  from "RDS".reporting$eod_trans_sum_table
+  where end_date BETWEEN starttime AND endtime
+  group by operation, currency, additional_info
+$function$
+"""
+
+STATIC_GRANTS_FOR_NEW_OBJECTS = """ALTER TABLE "reporting$eod_trans_sum_table" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p0" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p1" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p2" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p3" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p4" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p5" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p6" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p7" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p8" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p9" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p10" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p11" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p12" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p13" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p14" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p15" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p16" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p17" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p18" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p19" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p20" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p21" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p22" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p23" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p24" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p25" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p26" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p27" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p28" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p29" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p30" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p31" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p32" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p33" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p34" OWNER TO "RDS";
+ALTER TABLE "reporting$eod_trans_sum_table_p35" OWNER TO "RDS";
+
+ALTER FUNCTION "RDS"."end_of_day_transaction_summary_insert"(date,date) OWNER TO postgres;
+ALTER FUNCTION "Stanbic"."example$end_of_day_transaction_summary"(date,date) OWNER TO postgres;
+ALTER FUNCTION "global"."example$end_of_day_transaction_summary"(date,date) OWNER TO postgres;
+GRANT ALL ON FUNCTION "Stanbic"."example$end_of_day_transaction_summary"(date,date) TO "RDS";
+GRANT ALL ON FUNCTION "global"."example$end_of_day_transaction_summary"(date,date) TO "RDS";
+GRANT ALL ON FUNCTION "RDS"."end_of_day_transaction_summary_insert"(date,date) TO "RDS";
+"""
 
 # +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+
 # |C|L|A|S|S| |D|E|F|I|N|I|T|I|O|N|
@@ -105,6 +207,9 @@ class DBbox:
     port = "5432"
     conexion = None
     internalCursor = None
+    cursorLenght = 0
+    resultQuery = None
+    resultQueryClean = []
 
     def __init__(self,user,passwd,database,port):
         self.user = user
@@ -123,6 +228,14 @@ class DBbox:
         try:
             self.internalCursor = self.conexion.cursor()
             self.internalCursor.execute(query)
+            self.resultQuery = self.internalCursor.fetchall()
+
+            # - Codigo Limpia Shit
+            for i in self.resultQuery:
+                if type(i[0]) == unicode:
+                    self.resultQueryClean.append( i[0].encode('ascii') )
+
+            self.cursorLenght = len(self.resultQuery) 
         except Exception as err:
             print(err)
 
@@ -137,7 +250,6 @@ class PartitionBox:
     fin = ""
     table = ""
     FORMAT_STRING = "%Y-%m-%d"
-    
 
     def monthRange(self,year,month):
         self.date_rango = calendar.monthrange(year,month)
@@ -145,11 +257,9 @@ class PartitionBox:
         self.inicio = year + "-" + month + "-" + self.inicio
         self.fin = year + "-" + month + "-" + self.fin
 
-    def partitionNumber(self,partition):
-        pass        
-
     def stackPartition(self,table):
-        self.table = table + str(self.partition[0])
+        tempTuple , = self.partition
+        self.table = table + str(tempTuple[0])
         self.inicio = datetime.today().replace(day=1).strftime('%Y-%m-%d')
         self.data_rango = calendar.monthrange( datetime.now().year, datetime.now().month )
         self.fin = str(datetime.now().year) + '-' + str(datetime.now().month) + '-' + str(self.data_rango[1])
@@ -167,7 +277,9 @@ class QueryBuilder:
     finalQueryList = []
     childTablesList = []
     functionProcedureList = []
-    FORMAT_STRING = "%Y-%m-%d" 
+    FORMAT_STRING = "%Y-%m-%d"
+    schemasList = [] 
+    schemasQueries = []
 
     def addTuple(self,myTuple):
         self.finalQueryList.append(myTuple) 
@@ -206,6 +318,12 @@ class QueryBuilder:
                 firstOne = False
             else:
                 self.functionProcedureList.append('  ELSEIF ( NEW.end_date >= \'%s\' AND NEW.end_date < \'%s\' ) THEN INSERT INTO %s VALUES (NEW.*);' % ( inicio, fin, table ) )
+
+    def buildShemaQueries(self):
+        # - Build queries for schemas
+        for i in self.schemasList:
+            self.schemasQueries.append( 'ALTER FUNCTION "%s"."example$end_of_day_transaction_summary"(date, date) RENAME TO example$end_of_day_transaction_summary_orig;' % ( i ) + '\n' )
+            self.schemasQueries.append( 'CREATE OR REPLACE FUNCTION "%s"."example$end_of_day_transaction_summary"(starttime date, endtime date)' % ( i ) + '\n' + STATIC_NEW_EOD_FUNCTION_GENERAL )
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
@@ -266,40 +384,24 @@ else:
 # - Getting last partition
     db1.queryRDS(CURRENT_PARTITION_QUERY)
     pb1 = PartitionBox()
-    pb1.partition = db1.internalCursor.fetchone()
+    pb1.partition = db1.resultQuery
     pb1.stackPartition(TABLE_NAME_EOD_SUM_REPORT)
     pb1.addTuple()
 
-#print(pb1.partition)
-#print(pb1.table)
-#print(pb1.inicio)
-#print(pb1.data_rango)
-#print(pb1.fin)
+    # - Generate tuples in the list
+    qb1 = QueryBuilder()
+    qb1.addTuple( pb1.partitionTuple )
 
-#print( pb1.partitionTuple )
+    # - Generate child tables detail
+    qb1.generateChildTableQuery()
 
-qb1 = QueryBuilder()
-qb1.addTuple( pb1.partitionTuple )
-qb1.generateChildTableQuery()
-
-#print( qb1.finalQueryList )
-#print( qb1.childTablesList )
-#print( qb1.functionProcedureList )
-
-#print(help(pg8000))a
-
+    # - Get schemas to generate backups & procedures
+    db1.queryRDS(CURRENT_SCHEMAS_QUERY)
+    qb1.schemasList = db1.resultQueryClean
+    qb1.buildShemaQueries()
 
 fhb1 = FileHandlerBox()
-
 fhb1.touchOrOpenMyFile()
-
-#print( fhb1.currentQueryFileName )
-#print( fhb1.currentFile )
-
-#print( qb1.finalQueryList )
-#print( qb1.childTablesList )
-#print( qb1.functionProcedureList )
-
 fhb1.currentFile.write( STATIC_TABLE_PART_EOD_SUMM_1 )
 
 for i in qb1.childTablesList:
@@ -313,4 +415,13 @@ for i in qb1.functionProcedureList:
 
 fhb1.currentFile.write( STATIC_TABLE_PART_EOD_SUMM_3 )
 
+fhb2 = FileHandlerBox()
+fhb2.touchOrOpenMyFile()
+
+for i in qb1.schemasQueries:
+    fhb2.currentFile.write( i + '\n' )
+
+fhb2.currentFile.write( STATIC_GRANTS_FOR_NEW_OBJECTS )
+
 fhb1.closeMyFile()
+fhb2.closeMyFile()
